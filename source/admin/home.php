31708bc140f305acfc00f69efc252903707c2099
Mustafa Yasar <mmustafayyasar@gmail.com>
2021-05-06 18:15:02 +0300

    Home page of student created. Adding money to the wallet implemented.

--- source/admin/home.php
+++ source/admin/home.php
@@ -1,4 +1,8 @@
 <?php 
     session_start();
-echo $_SESSION['person_id'] . " " . $_SESSION['name'] . $_SESSION['surname'];
+	echo $_SESSION['person_id'] . " " . $_SESSION['name'] . $_SESSION['surname'];
+
+	$person_id = $_SESSION['person_id'];
+	$name = $_SESSION['name'];
+	$surname = $_SESSION['surname'];
  ?>
