bec9bb2f42dbe1177cf978e51eb66a8a014ebe49
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2021-05-14 16:13:01 +0300

    Wath lecture is fully functional, some layout changes will be made.

--- source/student/watch-lecture.php
+++ source/student/watch-lecture.php
@@ -16,10 +16,37 @@ $row1 = mysqli_fetch_array($result1,MYSQLI_ASSOC);
 
 
 # last lecture
+$sql2 = "SELECT MAX(lecture_id) FROM lecture WHERE course_id= '$course_id'";
+$result2 = mysqli_query($link, $sql2);
+$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+$islast = 0;
+
+if($row2["MAX(lecture_id)"]== $lecture_id){
+    $islast =1;
+}
 
 # first lecture
+$sql3 = "SELECT MIN(lecture_id) FROM lecture WHERE course_id= '$course_id'";
+$result3 = mysqli_query($link, $sql3);
+$row3 = mysqli_fetch_array($result3,MYSQLI_ASSOC);
+$isfirst = 0;
+
+if($row3["MIN(lecture_id)"]== $lecture_id){
+    $isfirst =1;
+}
 
 
+if (isset($_POST['create'])) {
+$note_text = $_POST['description'];
+$sql4 = "INSERT INTO note(student_id, lecture_id, course_id, note_text) VALUES ('$person_id', '$lecture_id', '$course_id', '$note_text')";
+
+    if (!mysqli_query($link, $sql4)) {
+        echo "ERROR : COULD NOT BE ADDED" . mysqli_error($link);
+    } else {
+        $note_id = mysqli_insert_id($link);
+        header("Location: watch-lecture.php");
+    }
+}
 
 
 
@@ -64,19 +91,87 @@ $row1 = mysqli_fetch_array($result1,MYSQLI_ASSOC);
 </nav>
 
 <div class="container">
+
+
+
+    <div class="dropdown">
+        <button class="btn btn-dark btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Select Note
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+            <?php
+            $query ="SELECT	lecture_id, note_text, note_id FROM note WHERE student_id='$person_id' AND lecture_id='$lecture_id'  AND course_id = '$course_id'";
+
+            if (!$query) {
+                printf("There is an error: %s\n", mysqli_error($link));
+                exit();
+            }
+            $result = mysqli_query($link, $query);
+            while($row = mysqli_fetch_array($result)){
+                echo "<li><a href=\"#\" data-value=$row[note_id]>$row[note_text]</a></li></a>";
+            }
+            echo "</select>";
+            ?>
+        </ul>
+    </div>
+
+
     <div class="jumbotron mt-4">
         <h3 class="display-5 mb-4"><?php echo $row1['lecture_name']?></h3>
     <img src="os.jpg" alt="Italian Trulli">
-        <form action='' method='post' id='formHiddenInputValue' name='formHiddenInputValue'>
-            <button type='button' class='btn btn-success' data-toggle='modal' id='play' name='play' value='play'>Play</button>
-        </form>
+
 
         <p class="lead mt-4">
-            <a class="btn btn-success btn-lg" href="pre-lecture.php" role="button">Previous Lecture</a>
-            <a class="btn btn-success btn-lg" href="next-lecture.php" role="button">Next Lecture</a>
+            <?php
+            if($isfirst == 1)
+            {
+                echo "<button type='button' disabled class='btn btn-secondary float-left'  data-dismiss='modal'><<</button>";
+            }
+
+            else
+            {
+                echo "<button type='submit' onclick=\"location.href='pre-lecture.php'\" name='pre-lecture' class='btn btn-success float-left'><<</button>";
+            }
+
+
+
+
+            if($islast == 1)
+            {
+                echo "<button type='button' disabled class='btn btn-secondary float-right'  data-dismiss='modal'>>></button>";
+            }
+
+            else
+            {
+                echo "<button type='submit' onclick=\"location.href='next-lecture.php'\" name='next-lecture' class='btn btn-success float-right'>>></button>";
+            }
+
+//            echo"<a class="btn btn-dark btn-lg" href="next-lecture.php" role="button">>></a>";
+
+            ?>
+
+        <form action='' method='post' id='formHiddenInputValue' name='formHiddenInputValue'>
+            <button type='button' class='btn btn-danger btn-lg' data-toggle='modal' id='play' name='play' value='play'>&#9658</button>
+        </form>
         </p>
     </div>
 </div>
+
+
+
+
+
+
+<form method="post">
+    <div class="form-group">
+        <label for="note">Take Note</label>
+        <textarea class="form-control" id="description" name="description" rows="3" placeholder="You can take notes" required="required" maxlength="200"></textarea>
+    </div>
+
+    <button type="submit" name="create" id="create" class="btn btn-primary">Create Note</button>
+    <div class="dropdown-divider"></div>
+
+</form>
 
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -100,8 +195,13 @@ $row1 = mysqli_fetch_array($result1,MYSQLI_ASSOC);
         });
     </script>
 
+<script>
+    $(".dropdown-menu li a").click(function(){
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
 
-
+</script>
 
 </html>
 
